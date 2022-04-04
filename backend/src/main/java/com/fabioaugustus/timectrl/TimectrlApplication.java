d9cdfd7d0689875ef679c04e9572223d31472a9f
Fabio Augustus <bywebbho.me@gmail.com>
2022-04-03 22:12:59 -0300

    Project created

---
+++ backend/src/main/java/com/fabioaugustus/timectrl/TimectrlApplication.java
@@ -0,0 +1,13 @@
+package com.fabioaugustus.timectrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimectrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimectrlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fabioaugustus/timectrl/TimectrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabioaugustus.timectrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimectrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
